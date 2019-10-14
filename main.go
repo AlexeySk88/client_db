a37c2f10deb381cd6a06baa203d40455003eae59
skale_x <skriplenok.pf@gmail.com>
2019-10-14 17:03:25 +0300

    поправлены каналы

--- main.go
+++ main.go
@@ -18,15 +18,10 @@ const (
 	query     string = "POST"
 	districts int    = 5
 	count     int    = 1000
-	chunk int = 20
+	chunk int = 1024
 )
 
 func main() {
-	//tr := &http.Transport{
-	//	MaxIdleConns:       20,
-	//	MaxIdleConnsPerHost:  20,
-	//}
-	//c := http.Client{Transport: tr}
 	c := http.Client{}
 	request = service.Request{
 		Client: c,
@@ -34,7 +29,6 @@ func main() {
 		Port:   port,
 		Query:  query,
 	}
-	arrayOrder := [count]orders.Order{}
 
 	newOrderChan := make(chan orders.Order, chunk)
 	orderChan := make(chan orders.Order, chunk)
@@ -52,36 +46,32 @@ func main() {
 	start := time.Now()
 	for i := 0; i < count; i++ {
 		rand.Seed(time.Now().UnixNano())
-		arrayOrder[i] = orders.Order{
+		order := orders.Order{
 			DistrictID: rand.Intn(districts) + 1,
 			Price:      float64(rand.Intn(20)*10 + 400),
 		}
-		fmt.Println(i)
-	}
-
-	for _, order := range arrayOrder {
 		newOrderChan <- order
 	}
 
 	for i := 0; i < count; i++ {
 		fmt.Println(i)
-		<-deliveryChan
+		<-orderChan
 	}
-
-
 
 	fmt.Println("the number of orders -", count)
 	fmt.Println("success", time.Now().Sub(start))
 }
 
 func doOrder(request service.Request, ch chan orders.Order, order chan orders.Order) {
-	curOrder := <-order
-	res := request.AddOrder(curOrder)
-	ch <- orders.Order{
-		OrderID: res.Order_id,
-		DistrictID: curOrder.DistrictID,
-		Price: curOrder.Price,
-		EntryIDs: res.Entry_id,
+	for {
+		curOrder := <-order
+		res := request.AddOrder(curOrder)
+		ch <- orders.Order{
+			OrderID: res.Order_id,
+			DistrictID: curOrder.DistrictID,
+			Price: curOrder.Price,
+			EntryIDs: res.Entry_id,
+		}
 	}
 }
 
